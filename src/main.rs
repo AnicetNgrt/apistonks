d34d839a258ef1612d6e9a2febcfaf6041fcb784
Anicet Ngrt <anicet.nougaret@zohomail.eu>
2021-03-08 11:32:12 +0100

    fixed bindings

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")
+    let listener = TcpListener::bind("0.0.0.0:8000")
         .expect("Failed to bind random port");
     
     // midly useless
